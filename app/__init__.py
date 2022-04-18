31b1df2b92f4ee36954716790bc8aeaba481c7bc
root <root@877595-cb00678.tmweb.ru>
2022-04-18 21:10:09 +0300

    initial commit

---
+++ app/__init__.py
@@ -0,0 +1,7 @@
+from flask import Flask
+import os
+from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__, static_folder="/root/app/static", template_folder='/root/app/templates')
+app.config.from_object('config')
+db = SQLAlchemy(app)
+from app import app, ClientController, model, security, CoffeController
